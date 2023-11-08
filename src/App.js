a02928e01af52e1e4f3a188d28231f393a138eba
rawanalmukhled <rawanalmekhled@gmail.com>
2023-11-08 20:17:57 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div>
-      <h1 className="bg-red-500">hi</h1>
-
-      <button className="btn btn-neutral">Neutral</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
